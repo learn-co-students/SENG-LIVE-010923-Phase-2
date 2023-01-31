2da11628b45f1998718a7a6fac00de5938e5d81e
Louis <Louis.Medina@flatironschool.com>
2023-01-31 14:40:45 -0500

    complete first deliverable, enable isDarkMode

--- 02_state_and_events /project_showcase/src/App.js
+++ 02_state_and_events /project_showcase/src/App.js
@@ -1,13 +1,37 @@
+// npm i => npm install
+// npm i && npm start => Command Sequence
+  // <some command> && <some other command>
+
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
 import projects from "./projects";
 
+// Import useState Hook
+import { useState } from 'react';
+
 const App = () => {
+
+
+  // Lifting Up State / Inverse Data Flow
+
+    // Set Up State / Setter Function
+    const [isDarkMode, isDarkModeSetter] = useState(true);
+
+    // Helper Function
+    function handleClick() {
+      isDarkModeSetter(!isDarkMode);
+    }
+
+  const appTheme = isDarkMode ? "App" : "App light";
+
   return (
-    <div className="App">
-      <Header />
+    <div className={appTheme}>
+      <Header 
+        isDarkMode={isDarkMode}
+        handleClick={handleClick}
+      />
       <ProjectForm />
       <ProjectList projects={projects} />
     </div>
--- 02_state_and_events /project_showcase/src/components/Header.js
+++ 02_state_and_events /project_showcase/src/components/Header.js
@@ -1,11 +1,49 @@
-const Header = () => {
+// Set Up State / Setter Function
+  // isOn / isOff
+  // isDarkMode => true / false
+// Set Up Our Helper Function
+// Set Up Our Event Listener on <button>
+
+// Import the useState Hook
+  // Boiler Plate / Built-In Function
+// import { useState } from 'react';
+
+const Header = ({ isDarkMode, handleClick }) => {
+  
+  // Set Up State / Setter Function
+
+  // Array Destructuring Assignment
+    // useState() => What Does It Return? (Array) 
+  
+  // const [ stateVariable, stateSetter ] = useState(initialValue);
+
+  // console.log(useState());
+
+    // Returns a New Array
+      // First Element => State
+      // Second Element => State Setter Function
+
+  // const [isDarkMode, isDarkModeSetter] = useState(true);
+
+  // // Helper Function
+  // function handleClick() {
+  //   isDarkModeSetter(!isDarkMode);
+  // }
+  
+  const buttonContent = isDarkMode ? "Light Mode" : "Dark Mode";
+
   return (
     <header>
       <h1>
         <span className="logo">{"//"}</span>
         Project Showcase
       </h1>
-      <button>Dark Mode</button>
+      
+      {/* Add Event Listener */}
+      
+      {/* Ternary Operator */}
+        {/* CONDITION ? IF TRUE : IF FALSE */}
+      <button onClick={handleClick}>{buttonContent}</button>
     </header>
   );
 }
