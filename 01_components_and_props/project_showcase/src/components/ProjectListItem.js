c9d038ccf676e58416bd829b75ee2de5c7480301
Diana Lin <linjdiana@gmail.com>
2023-01-31 23:53:47 -0600

    lecture 1 pushed

--- 01_components_and_props/project_showcase/src/components/ProjectListItem.js
+++ 01_components_and_props/project_showcase/src/components/ProjectListItem.js
@@ -1,13 +1,15 @@
 function ProjectListItem({project}) {
 
     // Object Destructuring
-    const { name, about, link} = project
+    const { name, about, link, image} = project
+
     return (
         <li>
         <h2>ProjectListItem Component</h2>
-        <p>{name}</p>
-        <p>{about}</p>
-        <p>{link}</p>
+        <p>Title: {name}</p>
+        <p>About: {about}</p>
+        <a href={link}>Project Link</a>
+        <p>{image}</p>
         </li>
     );
 }
