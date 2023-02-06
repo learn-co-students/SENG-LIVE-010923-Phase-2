26223c6e68df93e9b0004af998fa7f394ed2b883
Louis <Louis.Medina@flatironschool.com>
2023-02-06 14:21:21 -0500

    complete first deliverable, automatic fetching of projects

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -6,12 +6,12 @@ import ProjectList from "./components/ProjectList";
 // Deliverable 1: Implement useEffect in App component 
 // to load projects
 
-  // Import the `useEffect` hook from the React library
+  // DONE - Import the `useEffect` hook from the React library
 
-  // Invoke `useEffect` and make a `GET` request using 
+  // DONE - Invoke `useEffect` and make a `GET` request using 
   // the `fetch` method
 
-  // Update `projects` state upon successful response 
+  // DONE - Update `projects` state upon successful response 
   // from the server
 
 // Deliverable 2: Demonstrate the order of operations 
@@ -25,6 +25,8 @@ import ProjectList from "./components/ProjectList";
   // the component will occur 
 
 const App = () => {
+  console.log("Component Rendered!");
+  
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
@@ -33,11 +35,33 @@ const App = () => {
     setProjects(newProjectCollection);
   }
 
-  const handleClick = () => {
+
+  // Problem? => Component Will Continue Infinitely Re-Rendering
+    // Unconstrained Re-Rendering Behavior Due to Changes to "projects" State
+  
+  // const handleClick = () => {
+  useEffect(() => {
+    console.log("useEffect Behavior Fired!")
+    
+    // Optimistic Rendering
+    // setProjects(projects);
+
     fetch("http://localhost:4000/projects")
       .then((res) => res.json())
-      .then((projects) => setProjects(projects));
-  };
+      .then((projects) => { 
+        
+        // Pessimistic Rendering
+        setProjects(projects);
+      });
+  }, []);
+  // }
+
+  // Dependency Array
+    // Omit => Behavior Fires Off On Initial Render + With Each Subsequent Re-Render
+    // [] => Behavior Fires Off On Initial Render and Only ONCE
+    // 
+  
+  // };
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
@@ -53,7 +77,7 @@ const App = () => {
         onError={onError}
         projects={projects}
       />
-      <button onClick={handleClick}>Load Projects</button>
+      {/* <button onClick={handleClick}>Load Projects</button> */}
       <ProjectList projects={projects} />
     </div>
   );
