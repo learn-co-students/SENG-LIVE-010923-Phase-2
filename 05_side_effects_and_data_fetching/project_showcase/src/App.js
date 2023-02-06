967b8da7d777bca58b3d97bb3abadcb44691fd86
Louis <Louis.Medina@flatironschool.com>
2023-02-06 14:48:43 -0500

    create Timer component, enable counter incrementation, add clean up logic

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
+import Timer from "./components/Timer";
 
 // Deliverable 1: Implement useEffect in App component 
 // to load projects
@@ -12,23 +13,24 @@ import ProjectList from "./components/ProjectList";
   // the `fetch` method
 
   // DONE - Update `projects` state upon successful response 
-  // from the server
+  // from the server (PESSIMISTIC)
 
 // Deliverable 2: Demonstrate the order of operations 
 // between rendering a component and running the side 
 // effect
 
-  // Place a console.log() inside the `App` component as 
+  // DONE - Place a console.log() inside the `App` component as 
   // well as the `useEffect` method
 
-  // Open up the devtools to observe when each phase of 
+  // DONE - Open up the devtools to observe when each phase of 
   // the component will occur 
 
 const App = () => {
-  console.log("Component Rendered!");
+  // console.log("Component Rendered!");
   
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isTimerVisible, setIsTimerVisible] = useState(false);
 
   const onAddProject = newProject => {
     const newProjectCollection = [...projects, newProject];
@@ -41,7 +43,7 @@ const App = () => {
   
   // const handleClick = () => {
   useEffect(() => {
-    console.log("useEffect Behavior Fired!")
+    // console.log("useEffect Behavior Fired!")
     
     // Optimistic Rendering
     // setProjects(projects);
@@ -53,13 +55,25 @@ const App = () => {
         // Pessimistic Rendering
         setProjects(projects);
       });
+
+      // 3 Basic Phases of Component Lifecycle
+        // 1: Mounting (Initial Render)
+        // 2. Updating (Re-Rendering)
+        // 3. Unmounting (Removal of the Component from the DOM)
+
+      // Clean Up Function
+      return () => {
+        // Add Additional Behaviors to Fire Off Once Component
+        // Is Unmounted From the DOM
+      }
   }, []);
   // }
 
   // Dependency Array
     // Omit => Behavior Fires Off On Initial Render + With Each Subsequent Re-Render
     // [] => Behavior Fires Off On Initial Render and Only ONCE
-    // 
+    // [some, variable] => Behavior Fires Off On Initial Render + With Each Change to "some" / "variable"
+      // PATCH REQUESTS 
   
   // };
 
@@ -69,15 +83,23 @@ const App = () => {
   // With POST Request
   const onError = filteredList => { setProjects(filteredList) };
 
+  const handleClick = () => {
+    setIsTimerVisible(!isTimerVisible);
+  }
+
   return (
     <div className={isDarkMode ? "App" : "App light"}>
+      <button onClick={handleClick}>Toggle Timer</button>
+
+      {/* Conditional Rendering */}
+      { isTimerVisible ? <Timer /> : null } 
+
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
       <ProjectForm 
         onAddProject={onAddProject} 
         onError={onError}
         projects={projects}
       />
-      {/* <button onClick={handleClick}>Load Projects</button> */}
       <ProjectList projects={projects} />
     </div>
   );
--- 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -6,3 +6,43 @@
 
     // Open up the devtools to observe when the cleanup 
     // will occur during the stages of Component Lifecycle
+
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+    // Timer That Increments By One Each Second (1000 ms)
+    const [count, setCount] = useState(0);
+
+    // useEffect
+        // setTimeout / setInterval
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            
+            // Indicates a Memory Leak in Application
+            console.log("Behavior Still Firing!");
+            
+            // Callback Function As Argument for setCount
+                // Best Practice
+            // setCount(count + 1);
+            setCount(prevCount => prevCount + 1);
+        }, 1000);
+
+        // Clean Up Memory Leak
+            // Think About Behavior. Does It Continue Once Component Has
+            // Been Unmounted from the DOM?
+        return () => { 
+            console.log("Side Effect Cleaned Up!");
+            
+            clearInterval(intervalId);
+        };
+    }, [])
+    
+    return (
+        <>
+            <h1>My Timer</h1>
+            <p>Count: {count}</p>
+        </>
+    );
+}
+
+export default Timer;
